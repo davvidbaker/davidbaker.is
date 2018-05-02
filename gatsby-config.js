584592e7ab77ba1b436f1ec49d34f57f47855d8f
davvidbaker <davvidbaker@gmail.com>
2018-05-01 23:53:22 -0600

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-118580129-1'
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
