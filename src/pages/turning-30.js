b7e72ab26eb02b8d6a75036daeb370bf2fd634c5
david.baker <david.baker@workday.com>
2023-01-30 23:12:56 -0700

    fix images

--- src/pages/turning-30.js
+++ src/pages/turning-30.js
@@ -1,15 +1,18 @@
 import * as React from 'react'
 import "./turning-30.css"
 import { StaticImage } from "gatsby-plugin-image"
+import favicon from '../images/turning-30-favicon.png'
+import mainImage from '../images/turning-30.jpg'
+import slice from '../images/turning-30-slice.png'
 
-const Turning30 = () => <div><StaticImage src="../images/turning-30.jpg" alt="turning-30" /></div>
+const Turning30 = () => <div><StaticImage src={mainImage} alt="turning-30" /></div>
 
 export function Head() {
     return <>
         <meta property="og:title" content="davidbaker.is/turning-30" />
-        <meta property="og:image" content="/turning-30-slice.png" />
+        <meta property="og:image" content={slice} />
         <meta property="og:url" content="https://davidbaker.is/turning-30" />
-        <link rel="icon" href="/turning-30-favicon.png" type="image/x-icon" />
+        <link rel="icon" href={favicon} type="image/x-icon" />
         <title>davidbaker.is/turning-30</title>
     </>
 }
