133d73639b75a0982eea3a3ccbc3509694555707
david.baker <david.baker@workday.com>
2023-01-30 23:06:35 -0700

    head tags

--- src/pages/turning-30.js
+++ src/pages/turning-30.js
@@ -4,4 +4,14 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const Turning30 = () => <div><StaticImage src="../images/turning-30.jpg" alt="turning-30" /></div>
 
+export function Head() {
+    return <>
+        <meta property="og:title" content="davidbaker.is/turning-30" />
+        <meta property="og:image" content="/turning-30-slice.png" />
+        <meta property="og:url" content="https://davidbaker.is/turning-30" />
+        <link rel="icon" href="/turning-30-favicon.png" type="image/x-icon" />
+        <title>davidbaker.is/turning-30</title>
+    </>
+}
+
 export default Turning30
